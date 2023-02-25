f5b4ca8b5f2d16013fdc6de66b7a5697b1f8fdca
Debasish Bhola <devop69@gmail.com>
2023-02-25 18:13:46 +0530

    Update

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -14,7 +14,14 @@ public class MainActivity extends AppCompatActivity {
         EditText usernameEditText = (EditText) findViewById(R.id.usernameEditText);
         EditText passwordEditText = (EditText) findViewById(R.id.passwordEditText);
         Log.i("Username", usernameEditText.getText().toString());
+        String uName = usernameEditText.getText().toString();
+        String pass =  passwordEditText.getText().toString();
         Log.i("Password", passwordEditText.getText().toString());
+        if(uName .equals("Sibu") && pass.equals("Test")){
+            Toast.makeText(this, "Login Successfully", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
+        }
         Toast.makeText(this,usernameEditText.getText().toString() , Toast.LENGTH_SHORT).show();
         Toast.makeText(this,passwordEditText.getText().toString() , Toast.LENGTH_SHORT).show();
     }
